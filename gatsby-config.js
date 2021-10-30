57f5ce485e1e1aa2b454eb0f1eeea5208e8fd9f0
theodiablo <theodiablo@hotmail.com>
2021-10-30 14:30:48 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,59 +1,60 @@
+/*global __dirname, module*/
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://theo.camboulive.solutions",
-    title: "Théo Camboulive - Tech for Good",
-    author: "Théo Camboulive",
+    siteUrl: 'https://theo.camboulive.solutions',
+    title: 'Théo Camboulive - Tech for Good',
+    author: 'Théo Camboulive',
   },
   plugins: [
-    "gatsby-plugin-webpack-bundle-analyser-v2",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
+    'gatsby-plugin-webpack-bundle-analyser-v2',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
           families: [
-            "Josefin+Sans:400,300,300italic,400italic,700,700italic",
-            "Merriweather+Sans:400,700",
+            'Josefin+Sans:400,300,300italic,400italic,700,700italic',
+            'Merriweather+Sans:400,700',
           ],
         },
       },
     },
     {
-      resolve: "gatsby-plugin-google-gtag",
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        trackingIds: ["G-T8VW35DESW"],
+        trackingIds: ['G-T8VW35DESW'],
         gtagConfig: {
           anonymize_ip: true,
         },
       },
     },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
+        name: 'images',
         path: `${__dirname}/src/images/`,
       },
-      __key: "images",
+      __key: 'images',
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "pages",
+        name: 'pages',
         path: `${__dirname}/src/pages/`,
       },
-      __key: "pages",
+      __key: 'pages',
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -86,4 +87,4 @@ module.exports = {
     // Enable or disable Service Workers. Can be interesting?
     //"gatsby-plugin-offline",
   ],
-};
+}
